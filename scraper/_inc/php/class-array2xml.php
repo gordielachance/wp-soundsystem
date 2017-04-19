c72511b1d54af4e335e3660ec0249434ca0babad
rickybiscus <gordie.lachance@gmail.com>
2017-04-19 12:09:29 +0200

    array2xml class

---
+++ scraper/_inc/php/class-array2xml.php
@@ -0,0 +1,147 @@
+<?php
+
+class Array2XML {
+    
+    /* based (but tweaked) on
+     * https://github.com/easydigitaldownloads/Easy-Digital-Downloads/blob/master/includes/libraries/array2xml.php
+     */
+    
+    private static $xml = null;
+    private static $encoding = null;
+    private static $element_name = null;
+    /**
+     * Initialize the root XML node [optional]
+     * @param $version
+     * @param $encoding
+     * @param $format_output
+     */
+    public static function init($version = '1.0', $encoding = 'UTF-8', $element_name = 'element', $format_output = true) {
+        self::$xml = new DomDocument($version, $encoding);
+        self::$xml->formatOutput = $format_output;
+        self::$encoding = $encoding;
+        self::$element_name = $element_name;
+    }
+    /**
+     * Convert an Array to XML
+     * @param string $node_name - name of the root node to be converted
+     * @param array $arr - aray to be converterd
+     * @return DomDocument
+     */
+    public static function &createXML($arr=array(),$node_name) {
+        $xml = self::getXMLRoot();
+        $xml->appendChild(self::convert($arr,$node_name));
+        self::$xml = null;    // clear the xml node in the class for 2nd time use.
+        return $xml;
+    }
+    /**
+     * Convert an Array to XML
+     * @param string $node_name - name of the root node to be converted
+     * @param array $arr - aray to be converterd
+     * @return DOMNode
+     */
+    private static function &convert($arr=array(),$node_name) {
+        //print_arr($node_name);
+        $xml = self::getXMLRoot();
+        $node = $xml->createElement($node_name);
+        
+        //convert nodes to attributes if their name starts with @
+        if (is_array($arr)){
+            foreach($arr as $key => $value) {
+
+                if(substr($key,0,1) == '@'){
+                    $clean_key = ltrim($key, '@'); //strip prefix
+                    $arr['@attributes'][$clean_key] = $value;
+                    unset($arr[$key]);
+                }
+
+            }
+        }
+
+        if(is_array($arr)){
+
+            // get the attributes first.;
+            if(isset($arr['@attributes'])) {
+                foreach($arr['@attributes'] as $key => $value) {
+                    if(!self::isValidTagName($key)) {
+                        throw new Exception('[Array2XML] Illegal character in attribute name. attribute: '.$key.' in node: '.$node_name);
+                    }
+                    $node->setAttribute($key, self::bool2str($value));
+                }
+                unset($arr['@attributes']); //remove the key from the array once done.
+            }
+            // check if it has a value stored in @value, if yes store the value and return
+            // else check if its directly stored as string
+            if(isset($arr['@value'])) {
+                $node->appendChild($xml->createTextNode(self::bool2str($arr['@value'])));
+                unset($arr['@value']);    //remove the key from the array once done.
+                //return from recursion, as a note with value cannot have child nodes.
+                return $node;
+            } else if(isset($arr['@cdata'])) {
+                $node->appendChild($xml->createCDATASection(self::bool2str($arr['@cdata'])));
+                unset($arr['@cdata']);    //remove the key from the array once done.
+                //return from recursion, as a note with cdata cannot have child nodes.
+                return $node;
+            }
+        }
+        //create subnodes using recursion
+        if(is_array($arr)){
+            // recurse to get the node for that key
+            foreach($arr as $key=>$value){
+                
+                if (is_int($key)){ //dealing with <0/>..<n/> issues
+                    //$node->setAttribute('index', self::bool2str($key)); //TO FIX TO CHECK
+                    $key = self::$element_name;
+                }
+                
+                if(!self::isValidTagName($key)) {
+                    throw new Exception('[Array2XML] Illegal character in tag name. tag: '.$key.' in node: '.$node_name);
+                }
+                
+                if(is_array($value) && is_numeric(key($value))) {
+                    // MORE THAN ONE NODE OF ITS KIND;
+                    // if the new array is numeric index, means it is array of nodes of the same kind
+                    // it should follow the parent key name
+                    foreach($value as $k=>$v){
+                        $node->appendChild(self::convert($v,$key));
+                    }
+                } else {
+                    // ONLY ONE NODE OF ITS KIND
+                    $node->appendChild(self::convert($value,$key));
+                }
+                unset($arr[$key]); //remove the key from the array once done.
+            }
+        }
+        // after we are done with all the keys in the array (if it is one)
+        // we check if it has any text value, if yes, append it.
+        if(!is_array($arr)) {
+            $node->appendChild($xml->createTextNode(self::bool2str($arr)));
+        }
+        return $node;
+    }
+    /*
+     * Get the root XML node, if there isn't one, create it.
+     */
+    private static function getXMLRoot(){
+        if(empty(self::$xml)) {
+            self::init();
+        }
+        return self::$xml;
+    }
+    /*
+     * Get string representation of boolean value
+     */
+    private static function bool2str($v){
+        //convert boolean to text value.
+        $v = $v === true ? 'true' : $v;
+        $v = $v === false ? 'false' : $v;
+        return $v;
+    }
+    /*
+     * Check if the tag name or attribute name contains illegal characters
+     * Ref: http://www.w3.org/TR/xml/#sec-common-syn
+     */
+    private static function isValidTagName($tag){
+        $pattern = '/^[a-z_]+[a-z0-9\:\-\.\_]*[^:]*$/i';
+        return preg_match($pattern, $tag, $matches) && $matches[0] == $tag;
+    }
+}
