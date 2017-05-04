12c6ffb2484bfc7ae7dbe0ebd17f26dd2a38247b
rickybiscus <gordie.lachance@gmail.com>
2017-05-04 11:17:49 +0200

    doc

--- wp-soundsystem.php
+++ wp-soundsystem.php
@@ -5,7 +5,7 @@ Description: Manage a music library within Wordpress; including playlists, track
 Plugin URI: https://github.com/gordielachance/wp-soundsystem
 Author: G.Breant
 Author URI: https://profiles.wordpress.org/grosbouff/#content-plugins
-Version: 0.9.8.5
+Version: 0.9.8.6
 License: GPL2
 */
 
@@ -14,7 +14,7 @@ class WP_SoundSytem {
     /**
     * @public string plugin version
     */
-    public $version = '0.9.8.5';
+    public $version = '0.9.8.6';
     /**
     * @public string plugin DB version
     */
