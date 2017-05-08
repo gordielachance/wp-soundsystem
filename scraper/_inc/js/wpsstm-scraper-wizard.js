8b29683d44ed3c96cbfed96603f5913ab804c793
rickybiscus <gordie.lachance@gmail.com>
2017-05-08 21:30:55 +0200

    wizard : fixed advanced selectors not showing by default when filled

--- scraper/_inc/js/wpsstm-scraper-wizard.js
+++ scraper/_inc/js/wpsstm-scraper-wizard.js
@@ -2,21 +2,26 @@ jQuery(document).ready(function($){
     //tabs
     $("#wpsstm-wizard-tabs").tabs();
 
-    //regex
+    /*
+    advanced selectors
+    */
+    
+    //should we show it by default ?
+    $('.wpsstm-wizard-step-content .wpsstm-wizard-selector-advanced').each(function() {
+        var advanced_block = $(this);
+        var inputs_filled = advanced_block.find('input').filter(function () {
+            return !!this.value;
+        });
+        if ( inputs_filled.length > 0 ){
+            $(this).addClass('active');
+        }
+    });
+    
     $('.wpsstm-wizard-step-content a.wpsstm-wizard-selector-toggle-advanced').click(function(e) {
         e.preventDefault();
         var selector_row = $(this).parents('tr');
         var advanced_row = selector_row.find('.wpsstm-wizard-selector-advanced');
         advanced_row.toggleClass('active');
-    });
-    //default show regex
-    $('.wpsstm-wizard-step-content a.wpsstm-wizard-selector-toggle-advanced').each(function() {
-        var selector_row = $(this).parents('tr');
-        var advanced_row = selector_row.find('.wpsstm-wizard-selector-advanced');
-        var input = advanced_row.find('input');
-        if ( (input.val() != "") || input.hasClass('required') ){
-            advanced_row.addClass('active');
-        }
     });
     
 });
