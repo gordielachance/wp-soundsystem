0b08f215dba6b8380ba82a9cbf54f0cf844c90df
rickybiscus <gordie.lachance@gmail.com>
2017-05-11 09:08:07 +0200

    WIP shortenTable : able to run it several times on the same element

--- _inc/js/wpsstm-shortenTables.js
+++ _inc/js/wpsstm-shortenTables.js
@@ -8,23 +8,39 @@
         //reduce tracklists
         this.each(function() {
 
-            var rows = $(this).find(items_selector);
+            var wrapper = $(this);
+            var rows = wrapper.find(items_selector);
+            var current_visible_rows = wrapper.attr('data-visible-rows');
 
             if (rows.length == 0) return;
             if (rows.length <= max_rows) return;
 
-            var toggle_expand = $('<p class="wpsstm-toggle-tracklist-length"><button><i class="fa fa-angle-down" aria-hidden="true"></i></button></p>');
-            toggle_expand.insertAfter($(this));
-
+            rows.show();
             rows.slice(max_rows).hide();
 
-            toggle_expand.click(function(e) {
-                e.preventDefault();
-                // all trs with level-1 class inside abc table
-                rows.show();
-                //toggle_expand.find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
-                toggle_expand.hide();
-            });
+            if ( !current_visible_rows ){
+                var toggle_expand = $('<p class="wpsstm-toggle-tracklist-length"><button><i class="fa fa-angle-down" aria-hidden="true"></i></button></p>');
+                toggle_expand.insertAfter(wrapper);
+                toggle_expand.click(function(e) {
+                    e.preventDefault();
+                    
+                    if (!wrapper.hasClass('shortened-table-expanded')){
+                        // all trs with level-1 class inside abc table
+                        rows.show();
+                        toggle_expand.find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+                    }else{
+                        
+                    }
+                    wrapper.toggleClass('shortened-table-expanded');
+                    
+
+                });
+            }
+            
+            $(this).addClass('shortened-table');
+            $(this).attr('data-visible-rows',max_rows);
+
+
         });
        
        
