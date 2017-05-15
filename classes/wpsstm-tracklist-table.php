0408f4541b7067abfb6723e00ababc525d742335
rickybiscus <gordie.lachance@gmail.com>
2017-05-15 11:13:39 +0200

    minor

--- classes/wpsstm-tracklist-table.php
+++ classes/wpsstm-tracklist-table.php
@@ -294,7 +294,7 @@ class WP_SoundSytem_TracksList_Table{
                         <a href="<?php echo wpsstm_get_tracklist_link($this->tracklist->post_id);?>" target="_blank" class="wpsstm-tracklist-action-share"><i class="fa fa-share-alt" aria-hidden="true"></i> <?php _e('Share', 'wpsstm'); ?></a>
                         <a href="<?php echo wpsstm_get_tracklist_link($this->tracklist->post_id,true);?>" class="wpsstm-tracklist-action-xspf"><i class="fa fa-rss" aria-hidden="true"></i> <?php _e('XSPF', 'wpsstm'); ?></a>
 
-                        <a href="<?php echo wpsstm_get_tracklist_link($this->tracklist->post_id,true,true);?>" class="wpsstm-tracklist-action-xspf-download"><?php _e('Download XSPF', 'wpsstm'); ?></a>
+                        <a href="<?php echo wpsstm_get_tracklist_link($this->tracklist->post_id,true,true);?>" class="wpsstm-tracklist-action-xspf-download"><i class="fa fa-download" aria-hidden="true"></i> <?php _e('Download XSPF', 'wpsstm'); ?></a>
                         <?php
                     }
 
