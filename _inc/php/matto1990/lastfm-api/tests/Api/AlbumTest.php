e2ea5b650702b310a6b76ad55d7603b3b374e561
rickybiscus <gordie.lachance@gmail.com>
2017-05-16 01:29:38 +0200

    composer : require matto1990/lastfm-api

---
+++ _inc/php/matto1990/lastfm-api/tests/Api/AlbumTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Api;
+
+use LastFmApi\Api\AlbumApi;
+use LastFmApi\Exception\ApiFailedException;
+
+/**
+ * Tests album api calls
+ *
+ * @author Marcos Peña
+ */
+class AlbumTest extends BaseNotAuthenticatedApiTest
+{
+
+    private $albumApi;
+
+    const ALBUM_TITLE = 'Milo Goes to College';
+    const ALBUM_ARTIST = 'Descendents';
+
+    public function __construct($name = null, array $data = array(), $dataName = '')
+    {
+        if (!$this->isApiInitiated()) {
+            $this->initiateApi();
+        }
+        parent::__construct($name, $data, $dataName);
+        $this->albumApi = new AlbumApi($this->authentication);
+    }
+
+    public function testGetExistingInfo()
+    {
+        $albumInfo = $this->albumApi->getInfo(array(
+            'album' => self::ALBUM_TITLE,
+            'artist' => self::ALBUM_ARTIST));
+        // Assert
+        $this->assertArrayHasKey('name', $albumInfo);
+    }
+
+    public function testTracksInInfo()
+    {
+        $albumInfo = $this->albumApi->getInfo(array(
+            'album' => self::ALBUM_TITLE,
+            'artist' => self::ALBUM_ARTIST));
+        // Assert
+        $this->assertArrayHasKey('tracks', $albumInfo);
+    }
+
+    public function testGetNonExistingInfo()
+    {
+        try {
+            $this->albumApi->getInfo(array(
+                'album' => 'afadsffadfadf',
+                'artist' => 'daqfadfaldfa'));
+            $this->fail("Expected Album not found exception not thrown");
+        } catch (ApiFailedException $error) {
+            $this->assertEquals(6, $error->getCode());
+            $this->assertEquals("Album not found", $error->getMessage());
+        }
+    }
+
+    public function testSearch()
+    {
+        $searchResults = $this->albumApi->search(array(
+            'album' => self::ALBUM_TITLE)
+        );
+        $this->assertArrayHasKey('results', $searchResults);
+    }
+
+}
