e5a72d627c4eec71f86a38fbe9b27898c1b1e3e2
rickybiscus <gordie.lachance@gmail.com>
2017-05-20 11:20:09 +0200

    remove editor/author support for albums, artists & tracks

--- wpsstm-core-tracks.php
+++ wpsstm-core-tracks.php
@@ -256,7 +256,7 @@ class WP_SoundSytem_Core_Tracks{
             'labels' => $labels,
             'hierarchical' => false,
 
-            'supports' => array( 'title','editor','author','thumbnail', 'comments' ),
+            'supports' => array( 'title','thumbnail', 'comments' ),
             'taxonomies' => array( 'post_tag' ),
             'public' => true,
             'show_ui' => true,
