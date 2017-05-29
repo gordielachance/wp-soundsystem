715f9360975b6118a879a4200f1bd790bd74f171
rickybiscus <gordie.lachance@gmail.com>
2017-05-29 10:20:49 +0200

    indieshuffle.com preset

---
+++ scraper/presets/indieshuffle.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+Should try to support playlists and songs, eg.
+https://www.indieshuffle.com/playlists/best-songs-of-april-2017/
+https://www.indieshuffle.com/songs/hip-hop/
+*/
+
+class WP_SoundSytem_Playlist_IndieShuffle_Scraper extends WP_SoundSytem_Live_Playlist_Preset{
+
+    var $preset_slug = 'indie-shuffle';
+    
+    var $pattern = '~http(?:s)?://(?:www\.)?indieshuffle.com/(.+)~';
+    var $variables = array(
+        'uri' => null
+    );
+
+    var $options_default = array(
+        'datas_cache_min'   => 1440,
+        'selectors' => array(
+            'tracks'           => array('path'=>'#mainContainer .commontrack'),
+            'track_artist'     => array('attr'=>'data-track-artist'),
+            'track_title'      => array('attr'=>'data-track-title'),
+            'track_image'      => array('path'=>'img','attr'=>'src'),
+            'track_source_urls' => array('attr'=>'data-source-url'),
+        )
+    );
+
+    function __construct($post_id_or_feed_url = null){
+        parent::__construct($post_id_or_feed_url);
+        $this->preset_name = __('Indie Shuffle','wpsstm');
+    }
+
+}
--- scraper/wpsstm-scraper-presets.php
+++ scraper/wpsstm-scraper-presets.php
@@ -127,6 +127,7 @@ function wpsstm_register_scraper_presets($presets){
     require_once( $presets_path . 'spotify.php' );
     require_once( $presets_path . 'twitter.php' );
     require_once( $presets_path . 'reddit.php' );
+    require_once( $presets_path . 'indieshuffle.php' );
     
     $presets[] = new WP_SoundSytem_Playlist_LastFM_Scraper();
     $presets[] = new WP_SoundSytem_Playlist_Spotify_Playlist_Api();
@@ -142,6 +143,7 @@ function wpsstm_register_scraper_presets($presets){
     $presets[] = new WP_SoundSytem_Playlist_Twitter_Scraper();
     $presets[] = new WP_SoundSytem_Playlist_RTBF_Scraper();
     $presets[] = new WP_SoundSytem_Playlist_Reddit_Api();
+    $presets[] = new WP_SoundSytem_Playlist_IndieShuffle_Scraper();
     
     return $presets;
 }
