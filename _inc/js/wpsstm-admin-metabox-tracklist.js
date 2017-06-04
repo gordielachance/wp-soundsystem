ffde86172d229046566faa59231556f5c3a03ad4
rickybiscus <gordie.lachance@gmail.com>
2017-06-04 15:02:27 +0200

    track actions : live

--- _inc/js/wpsstm-admin-metabox-tracklist.js
+++ _inc/js/wpsstm-admin-metabox-tracklist.js
@@ -12,23 +12,23 @@ jQuery(function($){
         
         /* Row actions */
         //edit
-        wrapper.find('.row-actions .edit a').on("click", function(event){
+        wrapper.on( "click", ".row-actions .edit a", function(event) {
             event.preventDefault();
             var row = $(this).parents('tr');
             row.addClass('metabox-table-row-edit');
         });
         //save
-        wrapper.find('.row-actions .save a').on("click", function(event){
+        wrapper.on( "click", ".row-actions .save a", function(event) {
             event.preventDefault();
             wpsstm_tracklist_row_action(this);
         });
         //remove
-        wrapper.find('.row-actions .remove a').on("click", function(event){
+        wrapper.on( "click", ".row-actions .remove a", function(event) {
             event.preventDefault();
             wpsstm_tracklist_row_action(this);
         });
         //delete
-        wrapper.find('.row-actions .delete a').on("click", function(event){
+        wrapper.on( "click", ".row-actions .delete a", function(event) {
             event.preventDefault();
             wpsstm_tracklist_row_action(this);
         });
