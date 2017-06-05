3dfc46eb59b157c23b1666c33a7743ee6744566f
rickybiscus <gordie.lachance@gmail.com>
2017-06-05 16:27:08 +0200

    minor

--- wp-soundsystem.php
+++ wp-soundsystem.php
@@ -5,7 +5,7 @@ Description: Manage a music library within Wordpress; including playlists, track
 Plugin URI: https://github.com/gordielachance/wp-soundsystem
 Author: G.Breant
 Author URI: https://profiles.wordpress.org/grosbouff/#content-plugins
-Version: 1.0.1
+Version: 1.0.2
 License: GPL2
 */
 
