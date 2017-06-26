c75323ff6a896aadf150181c07d63b316b53718a
rickybiscus <gordie.lachance@gmail.com>
2017-06-27 01:53:25 +0200

    autosource_filter_cover_sources & autosource_filter_requires_artist (experimental) options

--- classes/wpsstm-track-class.php
+++ classes/wpsstm-track-class.php
@@ -344,6 +344,13 @@ class WP_SoundSystem_Track{
         $sources = apply_filters('wpsstm_get_track_sources_auto',$sources,$this,$args);
         
         $sources = $this->sanitize_track_sources($sources);
+        
+        if ( wpsstm()->get_options('autosource_filter_cover_sources') == 'on' ){
+            $sources = $this->autosource_filter_cover_sources($sources);
+        }
+        if ( wpsstm()->get_options('autosource_filter_requires_artist') == 'on' ){
+            $sources = $this->autosource_filter_title_requires_artist($sources);
+        }
 
         return $sources;
 
@@ -354,20 +361,49 @@ class WP_SoundSystem_Track{
     Except for tracks that have 'cover' in their title.
     */
     
-    function remove_cover_sources($sources){
+    function autosource_filter_cover_sources($sources){
         
-        //this track is a cover; abord
+        //this track IS a cover; abord
         $track_is_cover = stripos($this->title, 'cover');
         if ( $track_is_cover ) return $sources;
 
         foreach((array)$sources as $key=>$source){
-            $source_is_cover = stripos($source->title, 'cover');
+            $source_is_cover = stripos($source['title'], 'cover');
             if ( $source_is_cover ){
                 unset($source[$key]);
             }
         }
 
         return $sources;
+    }
+    
+    /*
+    Remove sources where that the track artist is not contained in the source title
+    */
+    
+    function autosource_filter_title_requires_artist($sources){
+
+        foreach((array)$sources as $key=>$source){
+            
+            /*TO FIX check if it works when artist has special characters like / or &
+            What if the artist is written a little differently ?
+            We should compare text somehow here and accept a certain percent match.
+            */
+            
+            $remove_sources = array();
+            
+            //sanitize data so it is easier to compare
+            $source_sanitized = sanitize_title($source['title']);
+            $artist_sanitized = sanitize_title($this->artist);
+
+            if (strpos($source_sanitized, $artist_sanitized) === false) {
+                wpsstm()->debug_log( json_encode( array('artist'=>$this->artist,'artist_sanitized'=>$artist_sanitized,'title'=>$this->title,'source_title'=>$source['title'],'source_title_sanitized'=>$source_sanitized),JSON_UNESCAPED_UNICODE ), "WP_SoundSystem_Track::get_track_sources_auto() - source ignored as artist is not contained in its title");
+                unset($sources[$key]);
+            }
+        }
+
+        return $sources;
+
     }
     
     function sort_sources_by_similarity($sources){
@@ -378,8 +414,11 @@ class WP_SoundSystem_Track{
         //compute similarity
         foreach((array)$sources as $key=>$source){
             
+            //sanitize data so it is easier to compare
             $source_title_sanitized = sanitize_title($source->title);
-            $source_title_sanitized = str_replace($artist_sanitized,"", $source_title_sanitized); //remove artist from source title so the string to compare is shorter
+            
+            //remove artist from source title so the string to compare is shorter
+            $source_title_sanitized = str_replace($artist_sanitized,"", $source_title_sanitized); 
             
             similar_text($source_title_sanitized, $title_sanitized, $similarity_pc);
             $sources[$key]->similarity = $similarity_pc;
@@ -396,29 +435,11 @@ class WP_SoundSystem_Track{
     function sanitize_track_sources($sources){
         
         if ( empty($sources) ) return;
-        
-        $artist_sanitized = sanitize_title($this->artist);
-        $title_sanitized = sanitize_title($this->title);
 
         foreach((array)$sources as $key=>$source){
             //url is not valid
-            if ( !$source['url'] || ( !filter_var($source['url'], FILTER_VALIDATE_URL) ) ) unset($sources[$key]);
-            
-            
-            
-            //check that the track artist is contained in the source title
-            /*
-            $source_sanitized = sanitize_title($source['title']);
-
-            if (strpos($source_sanitized, $artist_sanitized) === false) {
-                wpsstm()->debug_log( json_encode( array('artist'=>$this->artist,'title'=>$this->title,'source_title'=>$source['title']) ), "WP_SoundSystem_Track::get_track_sources_auto() - source ignored as artist is not contained in its title");
-                unset($sources[$key]);
-            }
-            */
-            
+            if ( !$source['url'] || ( !filter_var($source['url'], FILTER_VALIDATE_URL) ) ) unset($sources[$key]); 
         }
-        
-        $sources = $this->remove_cover_sources($sources);
 
         foreach((array)$sources as $key=>$source){
             $source = wp_parse_args($source,WP_SoundSytem_Source::$defaults);
