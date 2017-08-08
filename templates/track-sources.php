d93d76e3ec7e275a43d2a85fffb4e3fa12e494bc
rickybiscus <gordie.lachance@gmail.com>
2017-08-08 19:11:39 +0200

    minor

--- templates/track-sources.php
+++ templates/track-sources.php
@@ -8,18 +8,15 @@ $sources_query = $track->query_sources();
 if ( $sources_query->have_posts() ) { ?>
     <div class="wpsstm-track-sources-list">
         <?php
-        $source_position = 0;
         while ( $sources_query->have_posts() ) {
             
             $sources_query->the_post();
             global $wpsstm_source;
             
-            if ( !$source_link = $wpsstm_source->get_provider_link() ) continue;
-            echo $source_link;
+            if ( $source_link = $wpsstm_source->get_provider_link() ){
+                echo $source_link;
+            }
             
-            ?>
-
-            <?php
         }
         ?>
     </div>
