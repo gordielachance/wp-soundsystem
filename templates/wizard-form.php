be60ce5b87962bfa79641979c0cf4a6d658c3197
rickybiscus <gordie.lachance@gmail.com>
2017-08-16 10:24:37 +0200

    wizard : populate tracks BEFORE wizard notices

--- templates/wizard-form.php
+++ templates/wizard-form.php
@@ -2,6 +2,7 @@
 
 global $post;
 global $wpsstm_tracklist;
+$wpsstm_tracklist->populate_tracks(array('posts_per_page'=>-1)); //we must have the tracks populated before we output the notices
 
 wpsstm_wizard()->wizard_settings_init();
 $is_wizard_disabled = $wpsstm_tracklist->is_wizard_disabled();
