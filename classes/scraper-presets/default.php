8f0e3623c6c52843897feac1ac1b634b96146f3d
rickybiscus <gordie.lachance@gmail.com>
2017-11-16 13:53:00 +0100

    data-wpsstm-wizard-hover

--- classes/scraper-presets/default.php
+++ classes/scraper-presets/default.php
@@ -14,20 +14,6 @@ abstract class WP_SoundSystem_Live_Playlist_Preset extends WP_SoundSystem_Remote
         parent::__construct($post_id);
         
     }
-
-    function get_default_options(){
-        $defaults = parent::get_default_options();
-        return array_replace_recursive((array)$defaults,(array)$this->preset_options); //last one has priority
-    }
-
-    /*
-    Override this functions if your preset needs to filter the tracks.  
-    Don't forget the call to the parent function at the end.
-    */
-    
-    protected function validate_tracks($tracks){
-        return parent::validate_tracks($tracks);
-    }
     
     /*
     Checks that the preset can be used (eg. check for an API client ID)
@@ -41,6 +27,20 @@ abstract class WP_SoundSystem_Live_Playlist_Preset extends WP_SoundSystem_Remote
     */
     function can_load_feed(){
         return true;
+    }
+
+    function get_default_options(){
+        $defaults = parent::get_default_options();
+        return array_replace_recursive((array)$defaults,(array)$this->preset_options); //last one has priority
+    }
+
+    /*
+    Override this functions if your preset needs to filter the tracks.  
+    Don't forget the call to the parent function at the end.
+    */
+    
+    protected function validate_tracks($tracks){
+        return parent::validate_tracks($tracks);
     }
 
 }
