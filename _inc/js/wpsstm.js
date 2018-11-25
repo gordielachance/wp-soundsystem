dbb0532f92156cf8d0027340e3694cbec77f3866
gordielachance <gordie.lachance@gmail.com>
2018-11-25 22:23:34 +0100

    fix init tracklists either in iframes and in document
    this allow to init a track that is not within an iframe (eg ?tracklist_action=render)

--- _inc/js/wpsstm.js
+++ _inc/js/wpsstm.js
@@ -41,67 +41,92 @@
     });
     
     $( document ).ready(function() {
+        
+        var tracklists = $('.wpsstm-tracklist');
         var bottomPlayer = new WpsstmPlayer('wpsstm-bottom-player');
         var iframes = $('iframe.wpsstm-tracklist-iframe');
         var player_tracklists = [];
         
         var allLoaded = $.Deferred();
-        var countLoaded = 0;
-
+        
         /*
-        load iframes at init (fires only once)
+        load tracklists
         */
-        iframes.one( "load", function() {
-            
-            ++countLoaded;//increment
-            var iframe = $(this);
-            var iframe_el = $(this).get(0);
-            var iframe_index = iframes.index( iframe );
-
-            $(this).parents('.wpsstm-iframe-container').removeClass('wpsstm-iframe-loading');
-
-            var content = $(iframe_el.contentWindow.document.body);
-            var tracklist_html = $(content).find( ".wpsstm-tracklist" ).get(0);
-
-            var tracklist_obj = new WpsstmTracklist(tracklist_html,iframe_index);
-            player_tracklists.push(tracklist_obj);
-
-            wpsstm_debug("iframe tracklist #"+iframe_index+" populated");
-
-            //everything is loaded
-            if ( countLoaded == iframes.length ){
-                allLoaded.resolve();
-            }
-
-            //resize iframe
-            var content = $(iframe_el.contentWindow.document);
-            //var height = $(e.target).find('html').get(0).scrollHeight;
-            iframe.css('height',content.outerHeight());
-            console.log("resized iframe");
-
+        tracklists.each(function(index,tracklist_html) {
+            var tracklist_obj = new WpsstmTracklist(tracklist_html,index);
         });
         
-        allLoaded.done(function(v) {
-            bottomPlayer.debug('all iframes have been loaded');
-
-            //sort tracklists by tracklist index
-            function compare_tracklist_idx(a,b) {
-                if (a.index > b.index) return 1;
-                if (b.index > a.index) return -1;
-                return 0;
-            }
-
-            player_tracklists.sort(compare_tracklist_idx);
+        /*
+        load iframe tracklists
+        */
+        
+        if (!iframes.length){
+            allLoaded.resolve();
+        }else{
             
-            var allTracks = [];
-            $(player_tracklists).each(function(index,tracklist) {
-                allTracks = allTracks.concat(tracklist.tracks);
+            var iframesLoaded = $.Deferred();
+            var loadedIFramesCount = 0;
+
+            iframes.one( "load", function() {
+
+                ++loadedIFramesCount;//increment
+                var iframe = $(this);
+                var iframe_el = $(this).get(0);
+                var iframe_index = iframes.index( iframe );
+
+                $(this).parents('.wpsstm-iframe-container').removeClass('wpsstm-iframe-loading');
+
+                var content = $(iframe_el.contentWindow.document.body);
+                var tracklist_html = $(content).find( ".wpsstm-tracklist" ).get(0);
+
+                var tracklist_obj = new WpsstmTracklist(tracklist_html,iframe_index);
+                player_tracklists.push(tracklist_obj);
+
+                wpsstm_debug("iframe tracklist #"+iframe_index+" populated");
+
+                //all frames is loaded
+                if ( loadedIFramesCount == iframes.length ){
+                    iframesLoaded.resolve();
+                }
+
+                //resize iframe
+                var content = $(iframe_el.contentWindow.document);
+                //var height = $(e.target).find('html').get(0).scrollHeight;
+                iframe.css('height',content.outerHeight());
+                console.log("resized iframe");
+
             });
             
-            bottomPlayer.append_tracks(allTracks);
-            bottomPlayer.autoplay();
+            iframesLoaded.done(function(v) {
+                bottomPlayer.debug('all iframes have been loaded');
+
+                //sort tracklists by tracklist index
+                function compare_tracklist_idx(a,b) {
+                    if (a.index > b.index) return 1;
+                    if (b.index > a.index) return -1;
+                    return 0;
+                }
+
+                player_tracklists.sort(compare_tracklist_idx);
+
+                var allTracks = [];
+                $(player_tracklists).each(function(index,tracklist) {
+                    allTracks = allTracks.concat(tracklist.tracks);
+                });
+
+                bottomPlayer.append_tracks(allTracks);
+                bottomPlayer.autoplay();
+                
+                allLoaded.resolve();
+
+            });
             
+        }
+        
+        allLoaded.done(function(v) {
+            bottomPlayer.debug('all tracklists have been loaded');
         });
+
 
         
     });
