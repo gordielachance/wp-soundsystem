f662f5b6ee510375be978ce984cfab094db2d4b7
gordielachance <gordie.lachance@gmail.com>
2018-12-05 22:03:59 +0100

    Update wpsstm.js

--- _inc/js/wpsstm.js
+++ _inc/js/wpsstm.js
@@ -148,29 +148,6 @@ function wpsstm_debug(msg,prefix){
     }
 }
 
-function wpsstm_arr_diff (a1, a2) {
-
-    var a = [], diff = [];
-
-    for (var i = 0; i < a1.length; i++) {
-        a[a1[i]] = true;
-    }
-
-    for (var i = 0; i < a2.length; i++) {
-        if (a[a2[i]]) {
-            delete a[a2[i]];
-        } else {
-            a[a2[i]] = true;
-        }
-    }
-
-    for (var k in a) {
-        diff.push(k);
-    }
-
-    return diff;
-}
-
 function wpsstm_shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
