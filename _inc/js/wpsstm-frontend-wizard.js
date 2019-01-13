38d8bd5e4598ba046145ec26405907d108f4f6f1
gordielachance <gordie.lachance@gmail.com>
2019-01-13 14:00:37 +0100

    minor

--- _inc/js/wpsstm-frontend-wizard.js
+++ _inc/js/wpsstm-frontend-wizard.js
@@ -2,23 +2,25 @@ var $ = jQuery.noConflict();
 
 $( document ).ready(function() {
 
-    var shortcut_titles = $('#wpsstm-wizard-shortcuts li');
+    var shortcut_titles = $('#wpsstm-wizard-shortcuts li label');
     var clickable_shortcuts = shortcut_titles.filter(function( index ) {
-        return $( ".wpsstm-bang-desc", this ).length === 1;
+        var container = $(this).parents('li');
+        return $( ".wpsstm-bang-desc", container ).length === 1;
     });
     
     console.log(shortcut_titles);
     console.log(clickable_shortcuts);
     
     clickable_shortcuts.each(function() {
-        var title_el = $(this).find('label');
+        var container = $(this).parents('li');
         var desc_el = $(this).find('.wpsstm-bang-desc');
-        title_el.addClass('wpsstm-can-click');
+        $(this).addClass('wpsstm-can-click');
     });
     
     
     clickable_shortcuts.on( "click", function(e) {
-        var desc_el = $(this).find('.wpsstm-bang-desc');
+        var container = $(this).parents('li');
+        var desc_el = container.find('.wpsstm-bang-desc');
         desc_el.toggleClass('expanded');
     });
 
