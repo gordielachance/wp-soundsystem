8270498bc941f91fd0e7e5271526484a5f0af333
gordielachance <gordie.lachance@gmail.com>
2019-01-16 10:35:26 +0100

    minor

--- templates/tracklist.php
+++ templates/tracklist.php
@@ -23,7 +23,7 @@ if ($action=='render'){
 ?>
 
 <!DOCTYPE html>
-<html class="no-js wpsstm-no-admin-bar" <?php language_attributes(); ?>>
+<html class="no-js wpsstm-iframe-content" <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
