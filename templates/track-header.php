39b6ee75ed592cf2b0b664d5e7b250fc1e220bae
gordielachance <gordie.lachance@gmail.com>
2019-01-31 12:28:45 +0100

    track templates : better track header

--- templates/track-header.php
+++ templates/track-header.php
@@ -1,18 +1,58 @@
 <?php
 global $wpsstm_track;
 ?>
+<div class="wpsstm-track-notices">
+    <?php
+    /*
+    Notices
+    */
+    if ( $notices_el = WP_SoundSystem::get_notices_output($wpsstm_track->notices) ){
+        echo $notices_el;
+    }
+    ?>
+</div>
 <div class="wpsstm-track-info">
     <span class="wpsstm-track-artist" itemprop="byArtist" title="<?php echo $wpsstm_track->artist;?>"><?php echo $wpsstm_track->artist;?></span>
     <span class="wpsstm-track-title" itemprop="name" title="<?php echo $wpsstm_track->title;?>"><?php echo $wpsstm_track->title;?></span>
     <span class="wpsstm-track-album" itemprop="inAlbum" title="<?php echo $wpsstm_track->album;?>"><?php echo $wpsstm_track->album;?></span>
-    <?php 
-    if ($wpsstm_track->from_tracklist){
-        ?>
-        <span class="wpsstm-from-tracklist">
-            <?php _e('From:','wpsstm');?>
-            <a target="_parent" href="<?php echo get_permalink($wpsstm_track->from_tracklist);?>"><?php echo get_the_title($wpsstm_track->from_tracklist);?></a>
-        </span>
-        <?php
-    }
-    ?>
 </div>
+<?php 
+if ($wpsstm_track->from_tracklist){
+    ?>
+    <span class="wpsstm-from-tracklist">
+        <?php _e('From:','wpsstm');?>
+        <a target="_parent" href="<?php echo get_permalink($wpsstm_track->from_tracklist);?>"><?php echo get_the_title($wpsstm_track->from_tracklist);?></a>
+    </span>
+    <?php
+}
+/*
+Parent playlists
+*/
+if ( $playlists_list = $wpsstm_track->get_parents_list() ){
+
+    ?>
+    <div class="wpsstm-track-tracklists">
+        <strong><?php _e('In tracklists:','wpsstm');?></strong>
+        <?php echo $playlists_list; ?>
+    </div>
+    <?php
+}
+/*
+Favorited by
+*/
+if ( $loved_list = $wpsstm_track->get_favorited_by_list() ){
+    ?>
+    <div class="wpsstm-track-loved-by">
+        <strong><?php _e('Loved by:','wpsstm');?></strong>
+        <?php echo $loved_list; ?>
+    </div>
+    <?php
+}
+/*
+Track actions
+*/
+if ( $actions = $wpsstm_track->get_track_links() ){
+    $list = get_actions_list($actions,'track');
+    echo $list;
+}
+?>
