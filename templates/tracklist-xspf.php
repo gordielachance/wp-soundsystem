b902aec0c524f0fee4f15d5f9291427f1da57453
gordielachance <gordie.lachance@gmail.com>
2019-03-16 11:47:10 +0100

    bugfix

--- templates/tracklist-xspf.php
+++ templates/tracklist-xspf.php
@@ -19,7 +19,7 @@ require wpsstm()->plugin_dir . 'classes/wpsstm-playlist-xspf.php';
 $xspf = new mptre\Xspf();
 
 //playlist
-if ( $title = get_the_title(); ){
+if ( $title = get_the_title() ){
     $xspf->addPlaylistInfo('title', $title);
 }
 
