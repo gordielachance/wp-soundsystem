ce7546ba2fb704a93119d6f1d6ecdd96ed2701eb
gordielachance <gordie.lachance@gmail.com>
2019-04-01 00:36:04 +0200

    no need for $( document ).ready()
    since JS is enqueued in footer

--- _inc/js/wpsstm.js
+++ _inc/js/wpsstm.js
@@ -40,49 +40,43 @@ $('.wpsstm-artist-autocomplete').each(function() {
     });
 });
 
-$( document ).ready(function() {
-    wpsstm_debug("DOM READY!");
-    
-    //registration notice
-    if ( registration_notice = wpsstmL10n.registration_notice){
-        wpsstm_js_notice(registration_notice);
-    }
-    
-    //notices
+//registration notice
+if ( registration_notice = wpsstmL10n.registration_notice){
+    wpsstm_js_notice(registration_notice);
+}
 
-    $(document).on('click', 'a.wpsstm-close-notice', function(e) {
-        var notice = this.closest('.wpsstm-block-notice');
-        notice.remove();
-    });
-    
-    //queue tracks in player on tracklist init/refresh
-    var bottomPlayer = $('wpsstm-player#wpsstm-bottom-player').get(0);
-    if (bottomPlayer){
-        $(document).on( "wpsstmTracklistReady", function( event,tracklist ) {
-            bottomPlayer.queueContainer(tracklist);
+//notices
 
-        });
-    }
+$(document).on('click', 'a.wpsstm-close-notice', function(e) {
+    var notice = this.closest('.wpsstm-block-notice');
+    notice.remove();
+});
 
-    $('wpsstm-tracklist,.wpsstm-standalone-track').each(function(index,tracklist) {
-        
-        tracklist.setAttribute('id','wpsstm-tracklist-'+index);
-
-        if (tracklist.isExpired){
-            tracklist.reload_tracklist();
-        }else{
-
-            
-            /*
-            Since wpsstmTracklistReady is fired when the tracklist is inserted, it will be fired before document.ready.
-            So fire it once more at init.
-            */
-            $(document).trigger("wpsstmTracklistReady",[tracklist]);
-        }
+//queue tracks in player on tracklist init/refresh
+var bottomPlayer = $('wpsstm-player#wpsstm-bottom-player').get(0);
+if (bottomPlayer){
+    $(document).on( "wpsstmTracklistReady", function( event,tracklist ) {
+        bottomPlayer.queueContainer(tracklist);
 
     });
+}
 
-    
+$('wpsstm-tracklist,.wpsstm-standalone-track').each(function(index,tracklist) {
+
+    tracklist.setAttribute('id','wpsstm-tracklist-'+index);
+
+    if (tracklist.isExpired){
+        tracklist.reload_tracklist();
+    }else{
+
+
+        /*
+        Since wpsstmTracklistReady is fired when the tracklist is inserted, it will be fired before document.ready.
+        So fire it once more at init.
+        */
+        $(document).trigger("wpsstmTracklistReady",[tracklist]);
+    }
+
 });
 
 
