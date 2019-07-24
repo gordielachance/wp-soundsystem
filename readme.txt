52672ada6a8fb8e81325253981d4755f3948dcc9
gordielachance <gordie.lachance@gmail.com>
2019-07-24 11:16:50 +0200

    2.8.8 doc

--- readme.txt
+++ readme.txt
@@ -151,8 +151,10 @@ The community user is a Wordpress user you need to create, and that will be assi
 
 == Changelog ==
 
-= XXX =
-* abord tracklist reload when pressing ESC
+= 2.8.8 =
+* JS: Abord tracklist reload when pressing ESC
+* JS: Ability to add more than one track at once when editing a tracklist
+* Renamed import endpoint
 
 = 2.8.7 =
 * WPSSTM_Spotify_Endpoints & WPSSTM_Musicbrainz_Endpoints moved from API so we don't need two requests
--- wp-soundsystem.php
+++ wp-soundsystem.php
@@ -5,7 +5,7 @@ Description: Manage a music library within Wordpress; including playlists, track
 Plugin URI: https://api.spiff-radio.org
 Author: G.Breant
 Author URI: https://profiles.wordpress.org/grosbouff/#content-plugins
-Version: 2.8.7
+Version: 2.8.8
 License: GPL2
 */
 
@@ -36,7 +36,7 @@ class WP_SoundSystem {
     /**
     * @public string plugin version
     */
-    public $version = '2.8.7';
+    public $version = '2.8.8';
     /**
     * @public string plugin DB version
     */
