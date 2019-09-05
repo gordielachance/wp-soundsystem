22466bc9ed2312a5f9c42e3a4f25d8b1bf0192b3
gordielachance <gordie.lachance@gmail.com>
2019-09-05 12:59:50 +0200

    3.0.3

--- wp-soundsystem.php
+++ wp-soundsystem.php
@@ -5,7 +5,7 @@ Description: Manage a music library within Wordpress; including playlists, track
 Plugin URI: https://api.spiff-radio.org
 Author: G.Breant
 Author URI: https://profiles.wordpress.org/grosbouff/#content-plugins
-Version: 3.0.2
+Version: 3.0.3
 License: GPL2
 */
 
@@ -36,7 +36,7 @@ class WP_SoundSystem {
     /**
     * @public string plugin version
     */
-    public $version = '3.0.2';
+    public $version = '3.0.3';
     /**
     * @public string plugin DB version
     */
